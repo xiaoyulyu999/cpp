4d286f7c94d117d8a6999103390b57732477cc6d
xiaoyulyu999 <info@bloomora.net>
2025-11-29 21:47:11 +0000

    Initial

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <condition_variable>
+#include<queue>
+
+std::queue<int> q;
+std::condition_variable cv;
+std::mutex mutex;
+
+void producer() {
+    for (int i = 0; i < 10; ++i) {
+        {
+            std::unique_lock<std::mutex> ul(mutex);
+            q.push(i);
+            std::cout << "we pushing "<< i << std::endl;
+        }
+        cv.notify_one();
+    }
+}
+
+void consumer() {
+    for (int i = 0; i < 10; ++i) {
+        std::unique_lock<std::mutex> ul(mutex);
+        cv.wait(ul, [] { return !q.empty(); });//wait until queue is not empty(condition is true)
+        int item = q.front();
+        q.pop();
+        ul.unlock();
+        std::cout << "Consumed: " << item << std::endl;
+    }
+}
+
+
+int main () {
+    return 0;
+}
